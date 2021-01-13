db32a96e208eb0ed7757651b3cc77e447d40449f
bswan0002 <bswan0002@gmail.com>
2021-01-13 14:14:43 -0800

    ez

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  let textVar = document.getElementById("text");
+  textVar.innerHTML = "This is really cool!";
+});
